7b155554dae146b26114e51b2337f64c4e5c6982
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-12-12 02:59:07 +0900

    refactor : implement strike and ball assembler

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,15 +1,24 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class BaseballGame {
+    public HashMap<String,Integer> getResult(List<Integer> answer, List<Integer> userInput) {
+        HashMap<String,Integer> result = new HashMap<>();
+        int strike = strikeCounter(answer,userInput);
+        int ball = ballCounter(answer,userInput);
+        result.put("strike",strike);
+        result.put("ball", ball);
 
+        return result;
+    }
 
     public int strikeCounter(List<Integer> answer, List<Integer> userInput) {
         int strike = 0;
         for (int index = 0; index < 3; index++) {
-            if (answer.get(index) == userInput.get(index)) {
+            if (answer.get(index).equals(userInput.get(index))) {
                 strike++;
             }
         }
