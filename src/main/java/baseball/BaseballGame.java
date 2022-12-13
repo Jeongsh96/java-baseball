023678b29682a485143195533fa116205b1a52f2
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-12-14 00:53:17 +0900

    refactor: move two functions to BaseballGame
    moved the function that getting user input
    and number seperator

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,15 +1,58 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class BaseballGame {
-    public HashMap<String,Integer> getResult(List<Integer> answer, List<Integer> userInput) {
-        HashMap<String,Integer> result = new HashMap<>();
-        int strike = strikeCounter(answer,userInput);
-        int ball = ballCounter(answer,userInput);
-        result.put("strike",strike);
+
+    public static List<Integer> connectedNumbersDivider(int inputNumber) {
+        //LinkedHashMap<Integer, Integer> usersNumbers = new LinkedHashMap<Integer, Integer>();
+        List<Integer> userNumbers = new ArrayList<>();
+        int firstNumber = inputNumber / 100;
+        int secondNumber = inputNumber % 100 / 10;
+        int thirdNumber = inputNumber % 10;
+
+        userNumbers.add(firstNumber);
+        userNumbers.add(secondNumber);
+        userNumbers.add(thirdNumber);
+        return userNumbers;
+    }
+
+
+    public static void inputNumbersValidity(List<Integer> inputNumber) {
+        if(inputNumber.size()<3){
+            throw new IllegalArgumentException("[ERROR] 3자리의 수를 입력하세요");
+        }
+        if (inputNumber.get(0) > 9) { //입력된 세자리수가 999이상일 경우
+            throw new IllegalArgumentException("[ERROR] 3자리의 수를 입력하세요");
+        }
+        if (inputNumber.get(0) == 0 || inputNumber.get(1) == 0 || inputNumber.get(2) == 0) {  // 0이 포함되어 있을 경우
+            throw new IllegalArgumentException("[ERROR] 1-9 사이의 수를 입력하세요");
+        }
+        if (inputNumber.get(0).equals(inputNumber.get(1)) || inputNumber.get(1).equals(inputNumber.get(2)) || inputNumber.get(2).equals(inputNumber.get(0))) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 존재합니다.");
+        }
+    }
+
+    public  List<Integer> gameAnswer() {
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+        return answer;
+    }
+
+    public HashMap<String, Integer> getResult(List<Integer> answer, List<Integer> userInput) {
+        HashMap<String, Integer> result = new HashMap<>();
+        int strike = strikeCounter(answer, userInput);
+        int ball = ballCounter(answer, userInput);
+        result.put("strike", strike);
         result.put("ball", ball);
 
         return result;
@@ -36,4 +79,5 @@ public class BaseballGame {
         }
         return ball;
     }
+
 }
