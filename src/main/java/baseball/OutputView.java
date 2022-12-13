db87d18c04013067bd1b6124080d8283c37092f4
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-12-14 02:24:32 +0900

    refactor: move output parts to OutputView

--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -1,6 +1,12 @@
 package baseball;
 
+import java.util.HashMap;
+import java.util.List;
+
+import static baseball.BaseballGame.*;
+
 public class OutputView {
+
 
     public static void startGame(){
         System.out.println("숫자 야구 게임을 시작합니다");
@@ -10,5 +16,27 @@ public class OutputView {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
+    public static boolean printResult(List<Integer> answer, List<Integer>user){
+        HashMap<String,Integer> result = getResult(answer, user);
+        printHint(result.get("strike"),result.get("ball"));
+        if(result.get("strike").equals(3)){
+            return true;
+        }
+        return false;
+    }
 
+    public static void printHint(int strike, int ball){
+        if(strike>0 && ball ==0){
+            System.out.println(strike + "스트라이크");
+        }
+        if(strike==0 && ball >0){
+            System.out.println(ball + "볼");
+        }
+        if(strike ==0 && ball==0){
+            System.out.println("낫싱");
+        }
+        if(strike>0 && ball>0){
+            System.out.printf(ball+"볼"+"%d"+"strike\n",ball-strike);
+        }
+    }
 }
